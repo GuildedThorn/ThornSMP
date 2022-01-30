4b544bfeaa580084d7b455b6102460129277856a
Magix <kobedo11@gmail.com>
2022-01-29 20:26:32 -0500

    Update Config.java

--- src/main/java/com/guildedthorn/thornsmp/utils/Config.java
+++ src/main/java/com/guildedthorn/thornsmp/utils/Config.java
@@ -4,6 +4,6 @@ public final class Config {
     public Bot bot;
     
     public static class Bot {
-        public String token = "ODY4MjY0MzMyNjc1MzgzMzQ2.YPtILA.xGCc5WsDlabTeOKN_101HBl1olM", bridgeChannel = "";
+        public String token = "", bridgeChannel = "";
     }
 }
