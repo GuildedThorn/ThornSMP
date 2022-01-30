e6b01215f11c6447c80880f48ea4f93ec4ff19dd
Jamie Duddleston <guildedthorn@gmail.com>
2022-01-29 17:24:35 -0800

    Update Config.java

--- src/main/java/com/guildedthorn/thornsmp/utils/Config.java
+++ src/main/java/com/guildedthorn/thornsmp/utils/Config.java
@@ -4,6 +4,6 @@ public final class Config {
     public Bot bot;
     
     public static class Bot {
-        public String token = "OTM2Njc0NTMyMDQxODE4MTUy.YfQoIw.eMhpWFVe1BsROYd0oJNEn8eM15M", bridgeChannel = "936656719977791588";
+        public String token = "", bridgeChannel = "";
     }
 }
